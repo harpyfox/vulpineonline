358823961d0449d80d438c251b5ac09efd1bbff5
harper <harper.iro@gmail.com>
2025-09-01 22:52:00 +1000

    portoflio 1.3.1

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,74 +1,33 @@
-import * as m3u8Loader from "./m3u8-loader.js";
-import * as blueskySource from "./source-bluesky.js";
-
-//#region Types
-
-interface Entry {
-    text: string;
-    embed: (ImageEmbed | VideoEmbed | LinkEmbed)[] | null;
-}
-
-interface Embed {
-    type: string;
-    thumbnailUrl: string;
-    url: string;
-}
-
-interface ImageEmbed extends Embed {
-    type: "image";
-}
-
-interface VideoEmbed extends Embed {
-    type: "video";
-}
-
-interface LinkEmbed extends Embed {
-    type: "link";
-}
-
-interface EntryView {
-    element: HTMLElement;
-    entry: Entry;
-}
-
+// import * as m3u8Loader from "./m3u8-loader.js";
+// import * as blueskySource from "./source-bluesky.js";
 //#endregion
-
 //#region Variables
-
-const entryElementMap: EntryView[] = [];
-
+const entryElementMap = [];
 //#endregion
-
 //#region Functions
-
-function render(entryView: EntryView) {
+function render(entryView) {
     const entry = entryView.entry;
-
     if (entry.embed) {
         let parent = entryView.element;
- 
         for (let i = 0; i < entry.embed.length; i++) {
             const embed = entry.embed[i];
-            if (!embed) continue;
-
+            if (!embed)
+                continue;
             if (i != 0) {
-                const newParent = parent.cloneNode() as HTMLElement;
+                const newParent = parent.cloneNode();
                 if (newParent) {
                     //@ts-expect-error
                     parent.after(newParent);
                     parent = newParent;
-                } 
+                }
             }
             const a = parent.appendChild(document.createElement('a'));
             a.href = embed.url;
-
             switch (embed.type) {
-
                 case "image":
                     const img = a.appendChild(document.createElement('img'));
                     img.src = embed.thumbnailUrl;
                     break;
-
                 case "video":
                     const video = a.appendChild(document.createElement(`video`));
                     //video.autoplay = true;
@@ -78,80 +37,43 @@ function render(entryView: EntryView) {
                     video.muted = true;
                     video.playsInline = true;
                     //video.preload = "metadata";
-
                     video.poster = embed.thumbnailUrl;
                     video.src = embed.url;
-
                     let hls = null;
-                    if (video.src.endsWith('m3u8')) hls = m3u8Loader.loadAndAttach(video);
-
                     parent.onmouseenter = (event) => {
-                        if (hls && !hls.loadingEnabled) hls.startLoad();
                         video.play();
                         video.loop = true;
                         video.onended = null;
-                    }
+                    };
                     parent.onmouseleave = (event) => {
                         video.loop = false;
                         video.onended = (event) => {
                             video.currentTime = 0;
-                        }
-                    }
-                    
+                        };
+                    };
                     break;
-                
                 default:
                     break;
-
             }
-
             //const p = a.appendChild(document.createElement('p'));
             //p.innerText = `title`;
         }
     }
-
     //const p = entryView.element.appendChild(document.createElement('p'));
     //p.innerHTML = `${entry.text}`;
-
 }
-
 async function build() {
-
-    const name = `portfolio.build()`;
-    console.info(name);
-    console.time(name);
-
-    const blueskyEntries = await blueskySource.parse(`data-bluesky-src`);
-
-    const entryViews = [
-        blueskyEntries,
-    ].flat(1); 
-    
-    console.time(`portfolio.render()`);
-    for (const entryView of entryViews) {
-        render(entryView);
-        entryElementMap.push(entryView);
-    }
-    console.timeEnd(`portfolio.render()`);
-
-    console.timeEnd(name);
-    console.info(`${name} built ${entryElementMap.length} entries!`, entryElementMap);
-
     for (const video of document.getElementsByTagName('video')) {
-        
         video.muted = true;
         video.autoplay = true;
         video.loop = true;
-        video.controls = false;
+        if (!video.hasAttribute('controls')) {
+            video.controls = false;
+        }
         video.disablePictureInPicture = true;
     }
 }
-
 //#endregion
-
 //#region Export
-
 export { build };
-export type { EntryView, Entry, Embed, ImageEmbed, VideoEmbed, LinkEmbed };
-
 //#endregion
