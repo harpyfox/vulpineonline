a25976deabb5528c5758ddab0646cd7413925e59
harper <harper.iro@gmail.com>
2025-09-07 15:47:12 +1000

    hit counter!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
 import { WorkerEntrypoint } from "cloudflare:workers";
 export default class extends WorkerEntrypoint {
     async fetch(request) {
+        const cloudflare = request.cf;
+        if (request.headers.get("Sec-Fetch-Mode") == "navigate")
+            console.log(request);
         const url = new URL(request.url);
         if (url.pathname.startsWith("/api/")) {
-            const object = {
-                name: "i'm an api"
-            };
-            const response = new Response(JSON.stringify(object), {
-                headers: { "Content-Type": "application/json" },
-            });
-            return response;
+            return this.api(url);
         }
-        return this.env.ASSETS.fetch(request);
+        const response = await this.env.ASSETS.fetch(request);
+        this.tryPutHit(request, response); // don't await this!!
+        return response;
+    }
+    // hit counter!
+    async tryPutHit(request, response) {
+        if (request.headers.get("Sec-Fetch-Mode") != "navigate")
+            return;
+        if (!response.ok)
+            return;
+        const key = new URL(response.url).pathname;
+        let value = await this.env.KV.get(key, "json");
+        if (!value) {
+            value = {
+                hits: 0,
+            };
+        }
+        if (!value.hits)
+            value.hits = 0;
+        value.hits++;
+        this.env.KV.put(key, value);
+    }
+    async tryPutReferer() {
+    }
+    async api(url) {
+        const endpoint = url.pathname.slice("/api/".length);
+        const auth = false;
+        switch (endpoint) {
+            case "getHits":
+                // getHits?hostname=
+                break;
+            case "putHits":
+            default:
+                break;
+        }
+        const object = {
+            url: url.toString(),
+            endpoint: endpoint,
+        };
+        const response = new Response(JSON.stringify(object), {
+            headers: { "Content-Type": "application/json" },
+        });
+        return response;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,69 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 
 export type Env = {
     ASSETS: Fetcher;
+    KV: KVNamespace;
 };
 
 export default class<TEnv extends Env = Env> extends WorkerEntrypoint<TEnv> {
     override async fetch(request: Request) {
+        const cloudflare = request.cf;
+        if (request.headers.get("Sec-Fetch-Mode") == "navigate") console.log(request);
 
         const url = new URL(request.url);
         if (url.pathname.startsWith("/api/")) {
-            const object = {
-                name: "i'm an api"
-            };
-            const response = new Response(JSON.stringify(object), {
-                headers: { "Content-Type": "application/json" },
-            });
-            return response;
+            return this.api(url);
         }
 
-        return this.env.ASSETS.fetch(request);
+        const response = await this.env.ASSETS.fetch(request);
+
+        this.tryPutHit(request, response); // don't await this!!
+        
+        return response;
+    }
+
+    // hit counter!
+    async tryPutHit(request: Request, response: Response) {
+        if (request.headers.get("Sec-Fetch-Mode") != "navigate") return;
+        if (!response.ok) return;
+        
+        const key = new URL(response.url).pathname;
+
+        let value: any | null = await this.env.KV.get(key, "json");
+        if (!value) {
+            value = {
+                hits: 0,
+            };
+        }
+        if (!value.hits) value.hits = 0;
+
+        value.hits++;
+        this.env.KV.put(key, value);
+    }
+
+    async api(url: URL): Promise<Response> {
+
+        const endpoint: string = url.pathname.slice("/api/".length);
+        const auth = false;
+
+        switch (endpoint) {
+            case "getHits":
+                // getHits?hostname=
+
+                break;
+            case "putHits":
+        
+            default:
+                break;
+        }
+
+        const object = {
+            url: url.toString(),
+            endpoint: endpoint,
+        };
+        const response = new Response(JSON.stringify(object), {
+            headers: { "Content-Type": "application/json" },
+        });
+        return response;
+
     }
 }
